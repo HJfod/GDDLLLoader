f03d8bec81f53376a5939dd2fe0765824509d3f3
HJfod <60038575+HJfod@users.noreply.github.com>
2021-03-01 00:13:33 +0200

    YOOOO I ADDED THE VFS AND I GOT A NEW ERROR!!!!!!!!!!!!!!

--- src/gd/CustomListView.hpp
+++ src/gd/CustomListView.hpp
@@ -6,6 +6,16 @@
 
 class CustomListView : public cocos2d::CCLayer {
     public:
+        virtual void destructor() {
+            return reinterpret_cast<void(__thiscall*)(
+                CustomListView*, bool
+            )>(
+                ModLdr::base + 0x57ec0
+            )(
+                this, true
+            );
+        }
+
         virtual void setupList() {
             reinterpret_cast<void(__fastcall*)(
                 cocos2d::CCNode*
@@ -28,6 +38,31 @@ class CustomListView : public cocos2d::CCLayer {
             )(this, cell, ix);
         };
 
+        virtual void another_desctructor_wtf() {
+            return reinterpret_cast<void(__thiscall*)(
+                CustomListView*, bool
+            )>(
+                ModLdr::base + 0x658e8
+            )(
+                this, true
+            );
+        }
+
+        // dunno what vftable this is, prolly like scrolllayer or some shit
+        virtual void unknown0() {}
+        virtual void unknown1() {}
+        virtual void unknown2() {}
+        virtual void unknown3() {}
+        virtual void unknown4() {}
+        virtual void unknown5() {}
+        virtual void unknown6() {}
+
+        // prolly boomlistview vftable or some shit
+        virtual void unknown0_0() {}
+        virtual bool unknown1_0() { return true; }
+        virtual void unknown2_0() {}
+        virtual bool unknown3_0() { return true; }
+
         static CustomListView* create(cocos2d::CCArray* _arr, float _w, float _h, int _n) {
             
             // size of the listview is passed in xmm1 & xmm2
@@ -47,18 +82,18 @@ class CustomListView : public cocos2d::CCLayer {
             return ret;
         }
 
-        bool init(cocos2d::CCArray* _arr, float _w, float _h, CustomListView* _idk, int _n) {
+        bool init(cocos2d::CCArray* _arr, float _w, float _h, intptr_t _idk, int _n) {
 
             // width and height are stored in xmm registers
             __asm {
                 movss xmm2, [_h]
                 movss xmm3, [_w]
             }
-
+            
             std::cout << "dbg0\n";
 
             auto ret = reinterpret_cast<bool(__thiscall*)(
-                cocos2d::CCNode*, cocos2d::CCArray*, CustomListView*, int
+                cocos2d::CCNode*, cocos2d::CCArray*, intptr_t, int
             )>(
                 ModLdr::base + 0x10C20
             )(
@@ -67,7 +102,7 @@ class CustomListView : public cocos2d::CCLayer {
 
             // clean stack
             __asm add esp, 0xA
-
+            
             std::cout << "dbg1\n";
 
             return ret;
@@ -78,6 +113,12 @@ class CustomListView : public cocos2d::CCLayer {
 				ModLdr::base + 0x57e60
 			)(this);
 
+        }
+
+        void constructor() {
+            reinterpret_cast<void(__thiscall*)(CustomListView*)>(
+				ModLdr::base + 0x57e60
+			)(this);
         }
 
         ~CustomListView() {
@@ -94,14 +135,22 @@ class InheritedView : public CustomListView {
         }
 
         static InheritedView* create(cocos2d::CCArray* _a, float _w, float _h, int _bt) {
+            intptr_t dumbfuck;
+
             InheritedView* pRet = new InheritedView();
             // InheritedView is inherited from CustomListView
 
-            if (pRet && pRet->init(_a, _w, _h, nullptr, _bt)) {
-                std::cout << "yoot\n";
-                
-                pRet->autorelease();
-                return pRet;
+            __asm { mov dumbfuck, ecx }
+
+            if (pRet) {
+				pRet->constructor();
+                if (pRet->init(_a, _w, _h, dumbfuck, _bt)) {
+                    std::cout << "yoot\n";
+                    
+                    pRet->autorelease();
+                    return pRet;
+                } else
+                    pRet->destructor();
             }
             CC_SAFE_DELETE(pRet);
             return nullptr;
